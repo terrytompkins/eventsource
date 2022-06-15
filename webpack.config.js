80d980840bd98fdcd115c176da5254150f424ccb
Borja Gonzalez <borja@bgon.es>
2022-06-16 01:14:53 +0200

    update and configure webpack and scripts

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './lib/eventsource-polyfill.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'eventsource.js'
+  }
+}
