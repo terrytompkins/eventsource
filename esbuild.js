6234fa9ab2c05c53678ba4ba16d20f8c006a8cb2
Borja Gonzalez <borja@bgon.es>
2022-06-16 18:08:37 +0200

    back to webpack and fix config

--- esbuild.js
+++
@@ -1,9 +0,0 @@
-require('esbuild').build({
-  entryPoints: ['lib/eventsource-polyfill.js'],
-  bundle: true,
-  format: 'esm',
-  minify: true,
-  outfile: 'dist/eventsource.js',
-  target: 'es6',
-  platform: 'node',
-}).catch(() => process.exit(1))
